12afc40cc4818213e2e2d40c48315c2e4d4aa191
anthropomorphic <anthropomorphic@mikefdorst.com>
2013-07-22 16:52:39 -0400

    Fix security issue, add rspec

--- Gemfile
+++ Gemfile
@@ -24,7 +24,7 @@ group :doc do
 end
 
 group :production do
-  gem 'pg', '0.15.1'
+  gem 'pg', '0.15.1' # PostgreSQL (for Heroku deployment)
   gem 'rails_12factor', '0.0.2'
 end
 
--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -1,7 +1,27 @@
 # Be sure to restart your server when you modify this file.
 
-# Your secret key for verifying the integrity of signed cookies.
+# Your secret key is used for verifying the integrity of signed cookies.
 # If you change this key, all old signed cookies will become invalid!
+
 # Make sure the secret is at least 30 characters and all random,
 # no regular words or you'll be exposed to dictionary attacks.
-AnthroSite::Application.config.secret_token = '5647a74539dd3f6dcdc29267f2363b6d09b4322b6710e96c98eeb0202fa04db42add240d8e2835920bb3ca69306afa28ba67ee32e4ec5eab539fde493917c625'
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+require 'securerandom'
+
+def secure_token
+  token_file = Rails.root.join('.secret')
+  if File.exist?(token_file)
+    # Use the existing token.
+    File.read(token_file).chomp
+  else
+    # Generate a new token and store it in token_file.
+    token = SecureRandom.hex(64)
+    File.write(token_file, token)
+    token
+  end
+end
+
+AnthroSite::Application.config.secret_key_base = secure_token
