0821a07fdd3771287dfdf131ca95d1cefd52f624
anthropomorphic <anthropomorphic@mikefdorst.com>
2013-07-22 15:15:08 -0400

    Create Rails app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+AnthroSite::Application.config.secret_token = '5647a74539dd3f6dcdc29267f2363b6d09b4322b6710e96c98eeb0202fa04db42add240d8e2835920bb3ca69306afa28ba67ee32e4ec5eab539fde493917c625'
